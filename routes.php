1b56ed3b642b9690d94260ce380894cc5ab6a3e2
Janaina <janainapaulino32145@gmail.com>
2024-09-11 08:34:07 -0300

    add codigos para autenticação

--- routes.php
+++ routes.php
@@ -1,1 +1,12 @@
-  
+ <?php 
+ // inclui arquivos de controlador necessarios para lidar com diferentes acões  
+ require 'controllers/AuthControler.php'; // instancia o controlador de autenficação 
+ require 'controllers/UserController.php'; // instancia o controlador de usuario 
+ require 'controller/DashboardController.php'; // instancia o controlador de dashboard
+ 
+ // cria instãncias dos controladores para utilzar seus metodos 
+ $authController = new AuthController();
+ $userController = new UserController();
+ $dashboardController = new DashboardController();
+
+?>
