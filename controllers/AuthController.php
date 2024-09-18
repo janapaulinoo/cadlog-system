50496e4582d34e92c4a84e7f12d950c10ec46e46
Janaina <janainapaulino32145@gmail.com>
2024-09-18 07:38:44 -0300

    add codigos de registro o login

--- controllers/AuthController.php
+++ controllers/AuthController.php
@@ -1,3 +1,12 @@
 <?php 
 
-require 'models/user.php';
+require_once 'models/user.php';
+
+class AuthController
+{
+    //função responsavel pelo processo de login
+    public function login(){
+        
+    }
+}
+
---
+++ views/register.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastre-se</title>
+</head>
+<body>
+    <div>
+        <h2>Cadastro de Usuário</h2>
+        <form action="" method="post">
+            <label for="nome">Nome</label>
+            <input type="text" name="nome" id="nome" required>
+ 
+            <label for="email">Email</label>
+            <input type="email" name="email" id="email" required>
+ 
+            <label for="senha">Senha</label>
+            <input type="password" name="senha" id="senha" required>
+
+            <label for="">perfil:</label>
+            <select name="perfil" id="perfil">
+                <option value="admin">Admin</option>
+                <option value="gestor">Gestor</option>
+                <option value="colaborador">Colaborador</option>
+            </select> 
+            <butoon type="submit">Cadastrar</button>
+        </form>
+        <a href="">voltar ao login</a>
+    </div>
+</body>
+</html>
