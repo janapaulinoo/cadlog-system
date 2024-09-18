743ed0f35649faecd69a654e2e0a2b7db3647913
Janaina <janainapaulino32145@gmail.com>
2024-09-18 08:32:31 -0300

    add função de buscar email

--- models/user.php
+++ models/user.php
@@ -3,4 +3,20 @@
 //inclui o arquivo de conexão que contem a classe Databease para genreciar a conexão com o BD
 require_once 'models/database.php';
 
+class User
+{
+    // função para encontrar um usuario pelo email
+    public static function findByEmail($email){
+
+        //coleta a conexão com o bd 
+        $conn = Databse::getConnection();
+
+        $stmt = $conn->prepare("SELECT * FROM usuarios WHERE email = :email");
+        //executa a consulta com o email passado como parametro 
+        $stmt->execute(['email' => $email]);
+
+        //retorna  os dados do usuario encontrado como um array assosiativo 
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+}
 ?>
