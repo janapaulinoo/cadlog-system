9199ffcbee2f3b403cb949c697879aa566cc2e1a
Janaina <janainapaulino32145@gmail.com>
2024-09-30 10:18:49 -0300

    finalizado o projeto

--- views/login.php
+++ views/login.php
@@ -4,10 +4,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel='stylesheet' type='text/css' media='screen' href='style.css'>
     <title>Login</title>
 </head>
 <body>
     <main>
+        <h2>Login de Usuario <h2>
         <form action="index.php?action=login" method="post">
             <section>
                 <label for="email">Email</label>
